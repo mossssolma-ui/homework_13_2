75ab2e92dac5aec942541aadcca5599d5b604daa
Oleg Mat <mos_olma@mail.ru>
2025-11-18 13:02:54 +0300

    add func process_bank_operations

--- src/bank_operations.py
+++ src/bank_operations.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def process_bank_search(data: list[dict], search: str) -> list[dict]:
@@ -19,3 +20,24 @@ def process_bank_search(data: list[dict], search: str) -> list[dict]:
         if pattern.search(str(description)):
             filtered_data.append(transact)
     return filtered_data
+
+
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """
+    Функция принимает список словарей с данными о банковских операциях и список категорий операций.
+    Возвращает словарь, у которыго ключи - категории,
+    а значение - количество операций в каждой категории.
+    """
+    if not categories:
+        return {}
+
+    descriptions = [
+        d['description']
+        for d in data
+        if d.get('description') in categories
+    ]
+
+    counted = dict(Counter(descriptions))
+    result = {category: counted.get(category, 0) for category in categories}
+
+    return result
